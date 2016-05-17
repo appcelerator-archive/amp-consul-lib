5beb0bb13d41defa29024323f0798fd438557d11
bq <bquenin@axway.com>
2016-05-17 16:09:48 +0200

    Adding missing dist folder

---
+++ dist/Consul.js
@@ -0,0 +1,113 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function() {
+  function defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[ i ];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) {
+        descriptor.writable = true;
+      }
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
+
+  return function(Constructor, protoProps, staticProps) {
+    if (protoProps) {
+      defineProperties(Constructor.prototype, protoProps);
+    }
+    if (staticProps) {
+      defineProperties(Constructor, staticProps);
+    }
+    return Constructor;
+  };
+}();
+
+var _consul = require('consul');
+
+var _consul2 = _interopRequireDefault(_consul);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Consul = function() {
+  function Consul() {
+    _classCallCheck(this, Consul);
+
+    this.options = {
+      host: 'consul',
+      port: '8500',
+      promisify: true
+    };
+    this.client = (0, _consul2.default)(this.options);
+  }
+
+  _createClass(Consul, [ {
+    key: 'set',
+    value: function set(id, value) {
+      return regeneratorRuntime.async(function set$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              _context.next = 2;
+              return regeneratorRuntime.awrap(this.client.kv.set(id, value));
+
+            case 2:
+            case 'end':
+              return _context.stop();
+          }
+        }
+      }, null, this);
+    }
+  }, {
+    key: 'get',
+    value: function get(id) {
+      return regeneratorRuntime.async(function get$(_context2) {
+        while (1) {
+          switch (_context2.prev = _context2.next) {
+            case 0:
+              _context2.next = 2;
+              return regeneratorRuntime.awrap(this.client.kv.get(id));
+
+            case 2:
+              return _context2.abrupt('return', _context2.sent);
+
+            case 3:
+            case 'end':
+              return _context2.stop();
+          }
+        }
+      }, null, this);
+    }
+  }, {
+    key: 'delete',
+    value: function _delete(id) {
+      return regeneratorRuntime.async(function _delete$(_context3) {
+        while (1) {
+          switch (_context3.prev = _context3.next) {
+            case 0:
+              _context3.next = 2;
+              return regeneratorRuntime.awrap(this.client.kv.del(id));
+
+            case 2:
+              return _context3.abrupt('return', _context3.sent);
+
+            case 3:
+            case 'end':
+              return _context3.stop();
+          }
+        }
+      }, null, this);
+    }
+  } ]);
+
+  return Consul;
+}();
+
+exports.default = Consul;
---
+++ dist/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _Consul = require('./Consul');
+
+Object.defineProperty(exports, 'Consul', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_Consul).default;
+  }
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
